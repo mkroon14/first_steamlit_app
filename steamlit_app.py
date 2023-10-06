5994e4a5e882be36ae747d3e31648908b9f44e27
mkroon14 <147194988+mkroon14@users.noreply.github.com>
2023-10-06 14:40:31 -0400

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.subtitle('Breakfast Menu')
+streamlit.title('Breakfast Menu')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Hard-Boiled Free-Range Egg')
