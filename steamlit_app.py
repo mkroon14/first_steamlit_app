d88a511f243bb2a3eb439a910def10597b3a67d2
mkroon14 <147194988+mkroon14@users.noreply.github.com>
2023-10-11 09:47:57 -0400

    Update steamlit_app.py
    added fruit choice

--- steamlit_app.py
+++ steamlit_app.py
@@ -21,14 +21,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
 
 # normalize json file to only values
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # set the values into a table
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
