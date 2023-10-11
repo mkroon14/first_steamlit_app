98a1f9e07dffa30ca8e834b3025740be3ce51b4b
mkroon14 <147194988+mkroon14@users.noreply.github.com>
2023-10-11 09:44:53 -0400

    Update steamlit_app.py
    removed additional text

--- steamlit_app.py
+++ steamlit_app.py
@@ -24,7 +24,6 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
 
 # normalize json file to only values
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
