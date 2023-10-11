9bd1e144ecd11688fc353950a4a84a8d8f982162
mkroon14 <147194988+mkroon14@users.noreply.github.com>
2023-10-11 09:41:18 -0400

    Update steamlit_app.py
    added json

--- steamlit_app.py
+++ steamlit_app.py
@@ -24,4 +24,5 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
