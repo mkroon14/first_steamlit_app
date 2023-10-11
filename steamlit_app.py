01920306a410a20b4ae678dd7c4db2593ed1755f
mkroon14 <147194988+mkroon14@users.noreply.github.com>
2023-10-11 11:57:38 -0400

    Update steamlit_app.py
    removed additional row

--- steamlit_app.py
+++ steamlit_app.py
@@ -78,5 +78,3 @@ if streamlit.button('Add a Fruit to the List'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
-#write from streamlit
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
